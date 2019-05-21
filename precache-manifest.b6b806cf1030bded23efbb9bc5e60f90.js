9cc8ebe0d375559f95940c23767e5a3d791f4d3f
Mad Tea <43210154+nosidam48@users.noreply.github.com>
2019-05-21 19:31:54 +0200

    Updates

---
+++ precache-manifest.b6b806cf1030bded23efbb9bc5e60f90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c8b77651855ae82887440508165f06",
+    "url": "/lokal-portal-challenge/index.html"
+  },
+  {
+    "revision": "212ebd486a60bd238d22",
+    "url": "/lokal-portal-challenge/static/css/main.61346fbc.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/lokal-portal-challenge/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "212ebd486a60bd238d22",
+    "url": "/lokal-portal-challenge/static/js/main.ed984075.chunk.js"
+  },
+  {
+    "revision": "18e397568db76f5542ab",
+    "url": "/lokal-portal-challenge/static/js/runtime~main.f2209f89.js"
+  }
+]);
